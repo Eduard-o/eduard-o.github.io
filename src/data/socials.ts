0119ea3b8a1f349401cea927352c0308ecbbc3f6
Eduardo Abreu <abreu.eduardo.r@gmail.com>
2023-01-01 21:46:34 -0800

    Added socials

---
+++ src/data/socials.ts
@@ -0,0 +1,42 @@
+export const socialLinks = [
+  {
+    data_tip: 'GitHub',
+    href: 'https://github.com/Eduard-o',
+    aria_label: 'GitHub Profile',
+  },
+  {
+    data_tip: 'Twitter',
+    href: 'https://twitter.com/dev_abreu',
+    aria_label: 'Twitter Profile',
+  },
+  {
+    data_tip: 'Mastodon',
+    href: 'https://mas.to/@Eduard_o',
+    aria_label: 'Mastodon Profile',
+    svg: '',
+  },
+  {
+    data_tip: 'Discord',
+    href: 'https://discord.gg/aXe8X8UMKG',
+    aria_label: 'Discord Server Invite Link',
+    svg: '',
+  },
+  {
+    data_tip: 'Steam',
+    href: 'https://steamcommunity.com/id/Rutsy_',
+    aria_label: 'Steam Profile',
+    svg: '',
+  },
+  {
+    data_tip: 'YouTube',
+    href: 'https://www.youtube.com/@dev_abreu',
+    aria_label: 'YouTube Profile',
+    svg: '',
+  },
+  {
+    data_tip: 'Twitch',
+    href: 'https://www.twitch.tv/rutsy',
+    aria_label: 'Twitch Profile',
+    svg: '',
+  },
+];
